6fe8ac0dfea015bc6990d11e83e412c137a2787c
Brecht Verhoeve <46007524+bverhoeve@users.noreply.github.com>
2020-11-14 10:54:39 +0100

    Init StarbuzzCoffee exercise

---
+++ src/decorator/starbuzz/DarkRoast.java
@@ -0,0 +1,11 @@
+package decorator.starbuzz;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+        this.description = "Dark Roast Coffee";
+    }
+
+    public double cost() {
+        return 0.99;
+    }
+}
---
+++ src/decorator/starbuzz/StarbuzzCoffee.java
@@ -0,0 +1,18 @@
+package decorator.starbuzz;
+
+public class StarbuzzCoffee {
+ 
+	public static void main(String args[]) {
+		Beverage beverage = new Espresso();
+		System.out.println(beverage.getDescription() 
+				+ " $" + beverage.cost());
+ 
+		Beverage beverage2 = new DarkRoast();
+		System.out.println(beverage2.getDescription() 
+				+ " $" + beverage2.cost());
+ 
+		Beverage beverage3 = new HouseBlend();
+		System.out.println(beverage3.getDescription() 
+				+ " $" + beverage3.cost());
+	}
+}
