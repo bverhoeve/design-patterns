c3f7f2f028df6346346ea7c854f5e4e14b1d6a4f
bverhoeve <46007524+bverhoeve@users.noreply.github.com>
2020-12-12 10:56:39 +0100

    Facade home theater

---
+++ src/facade/hometheater/HomeTheaterTestDrive.java
@@ -0,0 +1,16 @@
+package facade.hometheater;
+
+public class HomeTheaterTestDrive {
+	public static void main(String[] args) {
+		Amplifier amp = new Amplifier("Amplifier");
+		Tuner tuner = new Tuner("AM/FM Tuner", amp);
+		StreamingPlayer player = new StreamingPlayer("Streaming Player", amp);
+		CdPlayer cd = new CdPlayer("CD Player", amp);
+		Projector projector = new Projector("Projector", player);
+		TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
+		Screen screen = new Screen("Theater Screen");
+		PopcornPopper popper = new PopcornPopper("Popcorn Popper");
+ 
+		
+	}
+}
