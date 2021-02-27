2feb1eeb4cc648919ad69591a720b846e219f0f2
bverhoeve <46007524+bverhoeve@users.noreply.github.com>
2021-02-27 11:03:14 +0100

    Voorbereiding

---
+++ src/iterator/dinermerger/Waitress.java
@@ -0,0 +1,32 @@
+package iterator.dinermerger;
+
+public class Waitress {
+	PancakeHouseMenu pancakeHouseMenu;
+	DinerMenu dinerMenu;
+ 
+	public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu) {
+		this.pancakeHouseMenu = pancakeHouseMenu;
+		this.dinerMenu = dinerMenu;
+	}
+ 
+	public void printMenu() {
+		// Prints every item on the menu
+
+	}
+ 
+	public void printVegetarianMenu() {
+		// Print the vegetarian menu
+	}
+ 
+	public void printLunchMenu() {
+		// Print the lunch menu
+	}
+
+	public void printBreakfastMenu() {
+		
+	}
+
+	public void isItemVegetarian(String name) {
+
+	}
+}
