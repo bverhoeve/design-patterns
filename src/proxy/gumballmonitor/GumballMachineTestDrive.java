7f8e61dcdb4db826fedf8c43c1772eb56b9066af
bverhoeve <46007524+bverhoeve@users.noreply.github.com>
2021-03-28 17:45:24 +0200

    gumball monitor

--- src/proxy/gumballmonitor/GumballMachineTestDrive.java
+++ src/proxy/gumballmonitor/GumballMachineTestDrive.java
@@ -5,46 +5,46 @@ public class GumballMachineTestDrive {
 	public static void main(String[] args) {
 		int count = 10;
 		String gumballName = "Machineo";
-	
         
 		GumballMachine gumballMachine = new GumballMachine(gumballName, count);
+		GumballMonitor monitor = new GumballMonitor(gumballMachine);
  
-		System.out.println(gumballMachine);
+		monitor.report();
 
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+		monitor.report();
 
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+		monitor.report();
 
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+		monitor.report();
 
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+		monitor.report();
 
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 		gumballMachine.insertQuarter();
 		gumballMachine.turnCrank();
 
-		System.out.println(gumballMachine);
+		monitor.report();
 
 	}
 }
